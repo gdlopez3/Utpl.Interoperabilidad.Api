282b5388817fda4fabb32e846d0816ccb6cd36ee
gdlopez3 <106038984+gdlopez3@users.noreply.github.com>
2023-05-03 20:52:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
 
 app = FastAPI()
 
+class Persona (BaseModel):
+    id: int
+    nombre: str
+    edad: int
+    ciudad: Optional[str] = None
+
+personasList = []
+
+@app.post("/personas", response_model=Persona)
+def crear_persona(person: Persona):
+    personasList.append(person)
+    return person 
+
+@app.get("/personas", response_model=List[Persona])
+def get_personas():
+            return personasList
+
+@app.get("/personas/{persona_id}", response_model=Persona)
+def obtener_persona(persona_id: int):
+    for persona in personasList:
+        if persona.id == persona_id:
+            return persona
+    raise HTTPException(status_code=404, detail="persona no encontrada")
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": "Interoperabilidad"}
