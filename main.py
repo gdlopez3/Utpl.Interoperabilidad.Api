27e6a7a3a6e9944cb82d9bc6b7d10d7d796f5d83
gdlopez3 Lopez <gdlopez3@utpl.edu.ec>
2023-05-20 16:17:45 +0000

    Actualizacion deber

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ def obtener_persona(persona_id: int):
 
 @app.get("/")
 def read_root():
-    return {"Hello": "TEST PARA LA APP EN LA NUBE ACTUALIZADO"}
+    return {"Hello": "TEST PARA LA APP EN LA NUBE ACTUALIZADO PARA CAPTURA DEBER"}
